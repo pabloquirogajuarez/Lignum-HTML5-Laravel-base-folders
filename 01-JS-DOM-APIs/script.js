b61bccf09ef566854a2c716f8c5da46eae1c9942
pabloquirogajuarez <pabloquirogajuarez@gmail.com>
2023-11-20 17:26:56 -0300

    update ejercicio 1.3.3

--- 01-JS-DOM-APIs/script.js
+++ 01-JS-DOM-APIs/script.js
@@ -6,15 +6,36 @@ document.addEventListener("DOMContentLoaded", function() { //
     // agrega una clase para activar el efecto de desvanecimiento
     section.classList.add("fade-in");
 
-    //button
+    //button 1.2
 
     function mostrarMensaje() {
         alert("soy un mensaje")
     }
 
-    var button = document.getElementById("button"); //selecciona el id "button"
+    //button 1.3
+    async function getData(){
+        try{
+            const data = await fetch('https://api.chucknorris.io/jokes/random'); //consumiendo api 
+            const json = await data.json();
+            
+           /* console.log(json);
+            console.log(json.value);*/ //obtiene los datos especificos del json en este caso "value"
+            parrafo.textContent = json.value;
+        }
+        catch(e){   
+            console.error(e);
+        }
 
-    //elemento.addEventListener(evento, función);
-    button.addEventListener("click", mostrarMensaje);
+    }
+
+    //selecciona el id "button"
+    var button = document.getElementById("button"); 
+
+    //elemento.addEventListener(evento, función); 
+    button.addEventListener("click", getData);
+
+    const parrafo = document.getElementById("mensajeDeChuck") //guardo el mensaje en el id
+
+
 });
 
